1a95a367bff83abcd93e2b790cc11de3b75ff8be
DHLau <ldhtrythebest@163.com>
2025-06-12 14:12:07 +0900

    setting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:ai_chat_robot/core/configs/theme/app_theme.dart';
+import 'package:ai_chat_robot/firebase_options.dart';
 import 'package:ai_chat_robot/presentation/splash/bloc/splash_cubit.dart';
 import 'package:ai_chat_robot/presentation/splash/pages/splash_page.dart';
 import 'package:ai_chat_robot/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -9,6 +11,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDependencies();
   runApp(const MyApp());
 }
